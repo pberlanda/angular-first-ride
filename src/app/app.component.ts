6f83a5492cc852bcffde49f21f5a4748af941758
pberlanda <paolo.berlanda@marconirovereto.it>
2022-03-07 20:28:13 +0100

    relazioni tra i vari componentt css html ecc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Il mio Angular!!!!';
+  name = 'Il mio progetto Angular!!!!';
 }
